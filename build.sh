42efecda2815b456fe39bbdbb5d50e00b7d96bc3
makutaku <14153280+makutaku@users.noreply.github.com>
2024-05-28 02:13:29 -0500

    Refactored how variables are passed to docker compose.

--- build.sh
+++ build.sh
@@ -1,33 +1,6 @@
 #!/bin/bash
 
-## Function to check environment variables and issue warnings
-#check_env_var() {
-#  local var_name="$1"
-#  # Check if the variable is set
-#  if [ -z "${!var_name}" ]; then
-#    echo "Error: $var_name is not set. Please set this environment variable." >&2
-#    # Check if running as superuser and the variable is missing
-#    if [ "$(id -u)" -eq 0 ]; then
-#      echo "Notice: Running as superuser. If $var_name is set in your usual environment, consider using 'sudo -E' to preserve it." >&2
-#    fi
-#
-#    exit 1
-#  fi
-#}
-#
-## Check both VAULT_ADDR and VAULT_TOKEN
-#check_env_var "VAULT_ADDR"
-#check_env_var "VAULT_TOKEN"
-
 echo "Building pysystemtrade_ecosystem"
-
-## Check for command-line argument and use it to override ENV if provided
-#if [ ! -z "$1" ]; then
-#  ENV="$1"
-#elif [ -z "$ENV" ]; then
-#  echo "Error: ENV variable is not set and no command-line argument provided."
-#  exit 1
-#fi
 
 BUILD_DIR="./build"
 if [[ -d "$BUILD_DIR" ]]; then
@@ -43,6 +16,8 @@ if [[ ! -d "$CONFIG_BUILD_DIR" ]]; then
   exit 1
 else
   cp -ra $CONFIG_BUILD_DIR/pysystemtrade_ecosystem/. "$BUILD_DIR"/
+  rm .env
+  ln -s ./build/env_files/$ENV/compose.env .env
 fi
 
 PYSYSTEMTRADE_PROJECT_NAME="pysystemtrade"
